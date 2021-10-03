fd87ae8119914256be46fe7893638f9617cc4edd
Brian Jesus Marzo Viviani <brian.jesus.marzo.viviani@gmail.com>
2021-10-03 00:24:01 -0300

    added TouchableWrapper and debounced navigation

---
+++ app.json.js
@@ -0,0 +1,10 @@
+/*
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+
+    has been removed from app.json to disable the expo splash screen
+
+*/
--- src/scenes/auth/sceneSplash.jsx
+++ src/scenes/auth/sceneSplash.jsx
@@ -1,19 +1,39 @@
-import React, { useEffect } from 'react'
-import { Text } from 'react-native'
+import React, { useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { Image, Text, View } from 'react-native'
 import PropTypes from 'prop-types'
+import TouchableWrapper from '../../components/global/layout/TouchableWrapper'
 import DefaultPage from '../../components/DefaultPage'
+const brianjesusokLogo = require('../../assets/logo.png');
 
 const SceneSplash = ({ navigation }) => {
-  useEffect(() => { setTimeout(() => { navigation.navigate('Login') }, 2000) }, [])
+  useFocusEffect(
+    useCallback(() => {
+      const delayedNavigation = setTimeout(() => {
+        navigation.navigate('Login')
+      }, 3000)
+      return () => clearTimeout(delayedNavigation)
+    }, [])
+  )
   return (
-    <DefaultPage>
-      <Text>
-        Splash
-      </Text>
-    </DefaultPage>
+    <TouchableWrapper handlePress={() => navigation.navigate('Login')}>
+      <DefaultPage>
+        <Image
+          style={{ width: 150, height: 150 }}
+          source={brianjesusokLogo}
+        />
+        <View>
+          <Text style={{ fontSize: 20 }}>
+            Moonlight Games
+          </Text>
+        </View>
+      </DefaultPage>
+    </TouchableWrapper>
   )
 }
 
-SceneSplash.propTypes = { navigation: PropTypes.object.isRequired }
+SceneSplash.propTypes = {
+  navigation: PropTypes.object.isRequired,
+}
 
-export default SceneSplash;
+export default SceneSplash
